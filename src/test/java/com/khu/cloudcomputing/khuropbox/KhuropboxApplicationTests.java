a222aa32e5a3f160ed504aa1d0f256422b076c2e
Mario76 <2001cupid@gmail.com>
2024-04-02 22:17:14 +0900

    Feat: Initiate back framework

---
+++ src/test/java/com/khu/cloudcomputing/khuropbox/KhuropboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khu.cloudcomputing.khuropbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhuropboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
